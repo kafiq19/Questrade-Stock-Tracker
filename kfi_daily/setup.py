e9533f312544c96339a3d46c00e56379a3c5aa92
kafiq19 <39530506+kafiq19@users.noreply.github.com>
2019-03-03 19:02:34 -0400

    Add files via upload

---
+++ kfi_daily/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='kfi_daily',
+      version='1.1.1',
+      description='equity tracking script',
+      url='https://github.com/csb-comren/data_processing/',
+      author='Khaleel Arfeen',
+      author_email='k.a@unb.ca',
+      license='MIT',
+      packages=['kfi_daily'],
+      install_requires=[
+          'openpyxl',
+          'requests',
+          'gsw==3.0.6',
+      ],
+      zip_safe=False)
